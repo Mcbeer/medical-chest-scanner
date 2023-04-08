943c14a3fae24ab31f52d80145d543704488e239
Nicolai Hornskov <nicolai.h.n@gmail.com>
2023-04-08 23:10:57 +0200

    chore: Added test case for GuidesListItem

--- frontend/src/components/GuidesList/GuidesList.tsx
+++ frontend/src/components/GuidesList/GuidesList.tsx
@@ -1,9 +1,8 @@
 import { useObservableGetState } from "observable-hooks";
-import { useContext } from "react";
+import { FC, useContext } from "react";
 import { BiChevronRightCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { GuideContext } from "../../context/GuideContext";
-import { DataModel } from "../../models/DataModel";
 import "./GuidesList.scss";
 
 export const GuidesList = () => {
@@ -13,19 +12,24 @@ export const GuidesList = () => {
   return (
     <div className="GuidesList">
       {guides.map((guide) => (
-        <GuideListItem key={guide.id + guide.name + guide.lang} guide={guide} />
+        <GuideListItem key={guide.id + guide.name + guide.lang} id={guide.id} name={guide.name} />
       ))}
     </div>
   );
 };
 
-const GuideListItem = ({ guide }: { guide: DataModel }) => {
+type Props = {
+  id: string;
+  name: string;
+}
+
+export const GuideListItem: FC<Props> = ({ id, name }) => {
   return (
     <div className="GuideListItem">
-      <Link to={guide.id}>
+      <Link to={id}>
         <div className="GuideListItem__content">
-          <div className="GuideListItem__id">{guide.id}</div>
-          <div className="GuideListItem__name">{guide.name}</div>
+          <div className="GuideListItem__id">{id}</div>
+          <div className="GuideListItem__name">{name}</div>
           <BiChevronRightCircle size="1.5rem" />{" "}
         </div>
       </Link>
